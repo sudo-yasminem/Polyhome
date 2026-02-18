65815fb8d80bf9dbad56590f3cd44ba9213d85ed
sudo-yasminem <you@example.com>
2026-02-18 18:28:15 +0100

    Fonction Envoi de commande à la maison fonctionnelle

--- app/src/main/java/com/yasm/polyhome/LightActivity.kt
+++ app/src/main/java/com/yasm/polyhome/LightActivity.kt
@@ -4,11 +4,9 @@ package com.yasm.polyhome
 
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.androidtp2.Api
 
 class LightActivity: AppCompatActivity() {
+
+    var listPeripheriquesTotal: List<PeriphData>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +27,34 @@ class LightActivity: AppCompatActivity() {
             insets
         }
 
+
         val tokenString = intent.getStringExtra("token")
+        val tokenClean = tokenString?.trim()?.replace("\"","")
+
+
 
         if(tokenString != null){
             Log.d("TOKEN_PERIPH", "Token OK pour LightActivity")
 
             //Test pour détécter les erreurs API de listPeriph
-            testAccess(tokenString)
+            testAccess(tokenClean)
             listPeriph(tokenString)
-            //ENSUITE LIST PERIPH
+
+            val bouton_onoff = findViewById<Button>(R.id.bouton_light_on1)
+            bouton_onoff.setOnClickListener {
+
+                val light1 = listPeripheriquesTotal?.find {
+                    it.type.contains("light")
+                }
+                if (light1 != null){
+                    toggleLight(light1, tokenClean)
+                }
+                else{
+                    Log.d("DEBUG_TOGGLE","Erreur Liste vide")
+                }
+
+
+            }
 
         }
         else{
@@ -45,9 +64,10 @@ class LightActivity: AppCompatActivity() {
 
     }
 
-    private fun listPeriph(tokenString: String){
+    private fun listPeriph(tokenString: String) {
 
-        val tokenClean = tokenString.trim().replace("\"","")
+        val tokenClean = tokenString.trim().replace("\"", "")?.replace("\n", "")
+
         Log.d("API_PERIPH", "Appel listPeriph - token: $tokenClean")
 
         Api().get<PeriphResponseData>("https://polyhome.lesmoulinsdudev.com/api/houses/683/devices", ::listPeriphSuccess, tokenClean)
@@ -61,6 +81,7 @@ class LightActivity: AppCompatActivity() {
             Log.d("API_PERIPH", "Retour listPeriphSuccess OK")
 
             val listePeripheriques = responsePeriph.devices
+            this.listPeripheriquesTotal = listePeripheriques
 
             listePeripheriques.forEach { periph ->
 
@@ -70,7 +91,7 @@ class LightActivity: AppCompatActivity() {
                     Log.d(tag, "Status Ouverture: ${periph.opening}")
                 }
                 if (periph.power != null){
-                    Log.d(tag,"Commandes : ${periph.availableCommands}")
+                    Log.d(tag,"Power status : ${periph.availableCommands}")
                 }
             }
         }
@@ -82,16 +103,50 @@ class LightActivity: AppCompatActivity() {
     }
 
 
-    private fun testAccess(tokenString: String){
-
-        val tokenTrim = tokenString.trim().replace("\"","")
-
-        Log.d("TEST_ACCESS", "Header envoyé: $tokenString")
+    private fun testAccess(tokenClean: String?) {
 
         Api().get<List<UserDataResponse>>("https://polyhome.lesmoulinsdudev.com/api/houses/683/users", { responseCode, users ->
            Log.d("TEST_ACCESS","Réponse TestAccess code: $responseCode - Users: $users")
-        }, tokenTrim)
+        }, tokenClean)
     }
+
+    private fun sendCommand(periphId: String, commandString: String, tokenClean: String?){
+
+        val houseId = "683" //A changer, récup propre d'un intent et pas hardcodé
+        val url = "https://polyhome.lesmoulinsdudev.com/api/houses/$houseId/devices/$periphId/command"
+
+        val requestData= CommandeData(commandString)
+        Log.d("API_COMMAND", "Commande envoyée: $commandString au periph: $periphId")
+
+        Api().post<CommandeData, Any>(url, requestData, ::sendCommandSuccess, tokenClean)
+    }
+
+    private fun sendCommandSuccess(responseCode: Int, responseCommand: Any?){
+        if(responseCode == 200){
+            Log.d("API_COMMAND", "Commande bien exécutée")
+            val tokenStringUpdate = intent.getStringExtra("token")
+            if(tokenStringUpdate != null){
+                listPeriph(tokenStringUpdate)
+            }
+        }
+        else{
+            Log.e("API_COMMAND", "Erreur code: $responseCode")
+        }
+    }
+
+    private fun toggleLight(periph: PeriphData, tokenClean: String?){
+        if(periph.type == "light"){
+            if(periph.power == 0){
+                val commande = "TURN ON"
+                sendCommand(periph.id, commande, tokenClean)
+            }
+            else{
+                val commande = "TURN OFF"
+                sendCommand(periph.id, commande, tokenClean)
+            }
+        }
+    }
+
 
 
 }
